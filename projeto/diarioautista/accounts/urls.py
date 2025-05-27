0daed4eb31623464506fb8b1f88c657464dc4faa
melvin473 <31097332+melvin473@users.noreply.github.com>
2025-05-26 21:17:03 -0300

    Update urls.py
    Versão final urls.py

--- projeto/diarioautista/accounts/urls.py
+++ projeto/diarioautista/accounts/urls.py
@@ -1,16 +1,16 @@
+Importação dos módulos necessários
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('service-worker.js', views.service_worker, name='service-worker'),
     path('login/', views.login_view, name='login'),  # Rota para a página de login
     path('perfil/', views.perfil, name='perfil'),  # Rota para a página de perfil
     path('editar/', views.perfil_editar, name='perfil_editar'),  # Rota para a página de edição do perfil
     path('cadastro/', views.cadastro_view, name='cadastro'), # Rota para a página de cadastro
-    path('perfil/diario/', views.diario, name='perfil_diario'),
-    path('perfil/diario/<int:postagem_id>/', views.visualizar_postagem, name='visualizar_postagem'),
-    path('perfil/diario/nova/', views.nova_postagem, name='nova_postagem'),
-    path('perfil/excluir', views.excluir_conta, name='excluir_conta'),
-    path('perfil/diario/postagem/<int:postagem_id>/excluir/', views.excluir_postagem, name='excluir_postagem'),
-    path('perfil/diario/relatorio', views.relatorio_analise, name='relatorio'),
+    path('perfil/diario/', views.diario, name='perfil_diario'), # Rota para a página do diário
+    path('perfil/diario/<int:postagem_id>/', views.visualizar_postagem, name='visualizar_postagem'), # Página de visualização de postagem
+    path('perfil/diario/nova/', views.nova_postagem, name='nova_postagem'), # Formulário de nova postagem
+    path('perfil/excluir', views.excluir_conta, name='excluir_conta'), # Função de exclusão da conta
+    path('perfil/diario/postagem/<int:postagem_id>/excluir/', views.excluir_postagem, name='excluir_postagem'), # Função de exclusão da postagem
+    path('perfil/diario/relatorio', views.relatorio_analise, name='relatorio'), # Exibição do relatório emocional
 ]
